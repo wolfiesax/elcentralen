8041d0e52ad7479d06d2b36d13b9f6ed2c4a10ea
pi <ulf.saxin@gmail.com>
2016-06-15 20:24:09 +0200

    Added bash scripts.

---
+++ gitSetup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Git
+git config --global user.name $USER
+git config --global user.email "ulf.saxin@gmail.com"
+git config --global core.editor "emacs"
+git config --global color.ui true
+git config --global color.status auto
+git config --global color.diff auto
+git config --global merge.tool kdiff3
+git config --global core.pager "less -r"
